6d75f1f7d3c0e59b2379ebf7e74f1b1b2e1885c1
SMARTHPHUTELA <smarthphutela9@gmail.com>
2025-09-02 18:58:26 +0530

    postModel Schema

---
+++ campusconnect/backend/models/postModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const postModel = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user", 
+    required: true,
+  },
+  caption: {
+    type: String,
+    default: "",
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user", 
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: String,
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("post", postModel)
