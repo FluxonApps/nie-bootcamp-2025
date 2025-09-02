c1f9cb55da6eb3ebb0281636968e6d3e5ea71ed0
SMARTHPHUTELA <smarthphutela9@gmail.com>
2025-09-02 19:12:55 +0530

    connectionModel updated (for friend  requests)

---
+++ campusconnect/backend/models/connectionModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connectionModel = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("connection", connectionModel);
