2316efe526446636f44dc831045fad8b8d548f36
Harshitha-79 <harshithakakubal@gmail.com>
2025-09-04 05:27:39 +0530

    conflict merged

--- campusconnect/frontend/src/App.tsx
+++ campusconnect/frontend/src/App.tsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import ConnectPeople from "./pages/ConnectPeople";
 import CreateGroup from "./pages/CreateGroup.tsx";
 import Profile from "./pages/Profile.tsx";
+import ConnectionsList from "./pages/ConnectionsList";
+import RequestsList from "./pages/RequestsList";
 
 const App: React.FC = () => {
   return (
@@ -17,6 +19,8 @@ const App: React.FC = () => {
         <Route path="/connect" element={<ConnectPeople />} />
         <Route path="/create-group" element={<CreateGroup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/connections" element={<ConnectionsList userId="68b82038c44844746251f832" />} />
+        <Route path="/requests" element={<RequestsList userId="68b7cbd7ec80daa0628be738" />} />
       </Routes>
     </Router>
   );
