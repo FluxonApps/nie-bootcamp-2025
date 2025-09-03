b7070fbf98c729b09e10517f234ec74464828e7c
Shreesha1564 <147400183+Shreesha1564@users.noreply.github.com>
2025-09-03 19:44:08 +0530

    Post model updates

--- campusconnect/backend/services/postService.js
+++ campusconnect/backend/services/postService.js
@@ -1,16 +1,36 @@
 const Post = require("../models/postModel");
 
 exports.getAllPosts = async () => {
-  const posts = await Post.find();
-  return posts || [];
+  return await Post.find()
+    .populate("user", "name username")
+    .populate("comments.user", "name username");
 };
 
-exports.addPost = async (post) => {
-  try {
-    const newPost = new Post(post);
-    return await newPost.save();
-  } catch (err) {
-    console.log("Error saving new post", err);
-    return;
+exports.addPost = async (postData) => {
+  const newPost = new Post(postData);
+  return await newPost.save();
+};
+
+exports.toggleLike = async (postId, userId) => {
+  const post = await Post.findById(postId);
+  if (!post) return null;
+
+  if (post.likes.includes(userId)) {
+    post.likes = post.likes.filter((id) => id.toString() !== userId.toString());
+  } else {
+    post.likes.push(userId);
   }
+
+  await post.save();
+  return { likeCount: post.likes.length, post };
+};
+
+exports.addComment = async (postId, userId, text) => {
+  const post = await Post.findById(postId);
+  if (!post) return null;
+
+  post.comments.push({ user: userId, text });
+  await post.save();
+
+  return await post.populate("comments.user", "name username");
 };
