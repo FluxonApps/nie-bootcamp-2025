45b202d26e3d10ad9a71351f2aa0a1b83b2cc946
spike-debug <rahulreddy.official.33@gmail.com>
2025-09-04 16:39:35 +0530

    conflict resolve

--- fintrack/backend/config/vars.js
+++ fintrack/backend/config/vars.js
@@ -1,10 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   APP_PORT: process.env.PORT ? process.env.PORT : "8003",
   DB_URL: process.env.DB_URL ? process.env.DB_URL : "",
-=======
-  APP_PORT: process.env.APP_PORT || 8003,   
-  DB_URL: process.env.DB_URL,               
-  SECRET_KEY: process.env.SECRET_KEY,       
->>>>>>> 96449592394042a7ec06d0c38145f04eb2eb4b8a
+  SECRET_KEY: process.env.SECRET_KEY,  
 };
