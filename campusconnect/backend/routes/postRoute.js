cfb864ab6692522eaba8028ce101dd6023930bc3
SMARTHPHUTELA <smarthphutela9@gmail.com>
2025-09-04 19:06:50 +0530

    post commits

--- campusconnect/backend/routes/postRoute.js
+++ campusconnect/backend/routes/postRoute.js
@@ -5,32 +5,32 @@ const postController = require("../controllers/postController");
 const postRoutes = [
   {
     method: "post",
-    url: "/posts",
+    url: "/api/posts",
     handler: upload.single("image") // middleware for image upload
   },
   {
     method: "post",
-    url: "/posts",
+    url: "/api/posts",
     handler: postController.createPost // actual handler
   },
   {
     method: "get",
-    url: "/posts",
+    url: "/api/get/posts",
     handler: postController.getAllPosts
   },
   {
     method: "post",
-    url: "/posts/:postId/like",
+    url: "/api/posts/:postId/like",
     handler: postController.likePost
   },
   {
     method: "post",
-    url: "/posts/:postId/comment",
+    url: "/api/posts/:postId/comment",
     handler: postController.commentPost
   },
   {
     method: "get",
-    url: "/posts/:postId/comments",
+    url: "/api/posts/:postId/comments",
     handler: postController.getComments // new route to fetch all comments
   }
 ];
