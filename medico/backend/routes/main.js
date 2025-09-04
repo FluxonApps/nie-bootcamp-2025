ef56c48ec2ca1f2a3ffa0b4e257bb3e693c6e3b4
Sumedha76 <sumedhahiremath@gmail.com>
2025-09-04 23:54:29 +0530

    Add Reviewdetails backend

--- medico/backend/routes/main.js
+++ medico/backend/routes/main.js
@@ -31,7 +31,12 @@ const allRoutes = [
         method: "PUT",
         path: `${base}/products/approve/:id`,
         handler: [productController.approveProduct],
-      }
+      },
+      {
+        method: "PATCH",
+        path: `${base}/products/:id/approve`,
+        handler: [productController.updateProductReview],
+  }
 ];
 
 module.exports = allRoutes;
